ea7bafe0d9aa8f6a307d0c5ce6eff8703af439da
Tech-A29 <tech-a29@Tech-A62s-MacBook-Air.local>
2019-05-24 09:52:23 +0100

    updated default_test

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,19 +1,14 @@
+
 # InSpec test for recipe mongodb::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-  describe service('mongod') do
-    it { should be_running }
-    it { should be_enabled }
-  end
+describe service "mongod" do
+it { should be_running }
+it { should be_enabled }
 end
 
-
-describe port(27017)  do # listening on mongodb port
-  it { should be_listening }
-end
-
-describe http('http://localhost', enable_remote_worker: true) do
-  its ('status') { should cmp 502 } # makes sure that it isnt a 502 error
+describe port(27017) do
+it { should be_listening }
 end
